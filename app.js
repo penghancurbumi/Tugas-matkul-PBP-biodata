e15a11cdab1bd357d28870f36225770be162d8d4
penghancurbumi <161696524+penghancurbumi@users.noreply.github.com>
2024-09-23 21:33:16 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const nama = documment.queryselector('#nama');
+nama.textcontent = "Muhammad Al Fakhreza Dwi Putra";
+
+const nim = documment.queryselector('#nim');
+nim.textcontent = "2023004035";
+
+const jurusan = document.queryselector('#jurusan');
+jurusan.textcontent = "Teknik Informatika";
+
+const TTL = document.queryselector('#TTL');
+TTL.textcontent = "Sukabumi, 13 April 2005";
+
+const alamat = document.queryselector('#alamat');
+alamat.textcontent = "Jl. Hj kokon Komariah,Subangjaya, Kec.Cikole, Kota Sukabumi";
+
